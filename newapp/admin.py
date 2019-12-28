bdc1fb7624eb13ae3f58323e4e6bd11686796eeb
webizfabulous <webizmoxx@gmail.com>
2019-12-28 17:26:49 +0545

    Added scientist and thoery models

--- newapp/admin.py
+++ newapp/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
 
-from .models import CustomUser
+from .models import (
+    CustomUser,
+    Theory,
+    Scientist
+)
 
-
+admin.site.register(Scientist)
+admin.site.register(Theory)
 admin.site.register(CustomUser)
+
 
--- newapp/models.py
+++ newapp/models.py
@@ -7,3 +7,30 @@ class CustomUser(AbstractUser):
 
 
 
+class Theory(models.Model):
+    name = models.CharField(max_length=100)
+    created = models.DateField(auto_now=False, auto_now_add=False)
+    created_with = models.ManyToManyField('Scientist',blank=True)
+    based_on = models.CharField(max_length=50)
+    details = models.TextField()
+    related_url = models.URLField(max_length=300)
+
+    def __str__(self):
+        return self.name 
+
+class Scientist(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField(auto_now=False, auto_now_add=False)
+    theories = models.ManyToManyField(Theory)
+    birth_place = models.CharField(max_length=50)
+    death = models.DateField(blank=True, auto_now=False, auto_now_add=False)
+    is_dead = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+    
+
+    

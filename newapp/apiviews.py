b18eac1b23447fbef66e5e9a9d63111c585accb5
webizfabulous <webizmoxx@gmail.com>
2019-12-28 17:42:30 +0545

    Added viewset

--- newapp/apiviews.py
+++ newapp/apiviews.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
+from .serializers import (
+    TheorySerializer,
+    ScientistSerializer
+)
+
+from .models import (
+    Theory,
+    Scientist
+)
+
+class TheoryViewSet(viewsets.ModelViewSet):
+    serializer_class = TheorySerializer
+    queryset = Theory.objects.all()
+
+
+class ScientistViewSet(viewsets.ModelViewSet):
+    serializer_class = ScientistSerializer
+    queryset = Scientist.objects.all()
 
